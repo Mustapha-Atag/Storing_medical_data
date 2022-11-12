a08a4fbf4b0422500d07d350abe72d2de52a5d92
unknown <atag10mustapha@gmail.com>
2022-11-13 00:17:55 +0100

    third commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+var account;
+
+window.addEventListener('load', async () => {
+	
+	if (typeof window.ethereum !== 'undefined') { 
+		console.log("MetaMask is Available :) !"); 
+	}
+			
+	// Modern DApp browsers
+	if (window.ethereum) {
+		window.web3 = new Web3(ethereum);
+			
+		// To prevent the page reloading when the MetaMask network changes
+		ethereum.autoRefreshOnNetworkChange = false;
+			
+		// To Capture the account details from MetaMask
+		const accounts = await ethereum.enable();
+		account = accounts[0];
+				
+	}
+		// Legacy DApp browsers
+	else if (window.web3) {
+			//window.web3 = new Web3(web3.currentProvider);
+			window.web3 = new Web3(new Web3.providers.HttpProvider(infura_endPoint));
+	}
+		// Non-DApp browsers
+	else {
+		console.log('Non-Ethereum browser detected. Please install MetaMask');
+	}
+			
+});
+
+
+var infura_endPoint = "https://sepolia.infura.io/v3/81fd111a871d4790b8e1109eabf8087a";
+
 var hospital_contract_addr = "0xAaDEFF05081634079c91E0ac43488eba7f320B2F";
 
 var hospital_abi = [
